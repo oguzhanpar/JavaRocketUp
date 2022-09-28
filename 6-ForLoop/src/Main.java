ec78d9b27aa1df3b6371377416f340ecb3f54e4f
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-28 18:06:12 +0300

    --ForLoop--

---
+++ 6-ForLoop/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Loop is starting !");
+
+		for (int i = 0; i < 20; i++) {
+
+			System.out.println("Number is:" + i);
+
+		}
+
+		System.out.println("Loop is completed !");
+
+	}
+
+}
