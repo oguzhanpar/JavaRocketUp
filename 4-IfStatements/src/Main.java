bff56ce8a97cf32cbf9543b8b4d638257b09fd80
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-28 17:42:46 +0300

    --IfStatements--

---
+++ 4-IfStatements/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int number = 20;
+
+		if (number > 40) {
+
+			// number is greater than 40 here runs
+			System.out.println("Number is greater than 40");
+
+		} else if (number == 20) {
+
+			// number is equal to here runs
+			System.out.println("Number is equal to 20");
+
+		} else {
+
+			// number is less than 40 here runs
+			System.out.println("Number is less than 40");
+
+		}
+
+	}
+
+}
