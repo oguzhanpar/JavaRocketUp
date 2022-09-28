2c69a76b0c42d99fcd6612109d0fc18bed67f002
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-28 17:00:17 +0300

    ---DataTypes--

---
+++ 3-DataTypes/src/Main.java
@@ -0,0 +1,37 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		//Primitive data types: The primitive data types include boolean, char, byte, short, int, long, float and double.
+		
+		//1
+		boolean result = false; // boolean returns true or false | Default Size: 1 bit
+
+		//2
+		char c = '\u0000'; // value-range lies between '\u0000' (or 0) to '\uffff' |  Stores a single character/letter or ASCII values | Default Size: 2 byte
+
+		//3
+		byte b = 0; // -128(Minimum Value) | 127(Maximum Value) | Default Size: 1 byte
+
+		//4
+		short s = 0; // -32,768(Minimum Value) | 32,767(Maximum Value) | Default Size: 4 byte
+
+		//5
+		int number = 0; // -2,147,483,648(Minimum Value) | 2,147,483,647(Maximum Value) | Default Size: 4 byte
+
+		//6
+		long l = 0; // -9,223,372,036,854,775,808(-2^63)(Minimum Value) | 9,223,372,036,854,775,807(2^63 -1)(Maximum Value) | Default Size: 8 byte		
+
+		//7
+		float f = 234.5f; //value range is unlimited | Default Size: 4 byte 
+
+		//8
+		double d = 17.2; //value range is unlimited | Default Size: 4 byte 
+
+		
+		
+	}
+
+}
