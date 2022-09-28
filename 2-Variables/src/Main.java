4f61eec020fa55f3e2be9f611c6976f24e3617d0
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-28 16:23:05 +0300

    --Variables--

--- 2-Variables/src/Main.java
+++ 2-Variables/src/Main.java
@@ -4,7 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
+		String firstName = "Oğuzhan";
+		String lastName = "Parlak";
+		int age = 27;
 		
+		System.out.println("First Name: " + firstName);
+		System.out.println("Last Name: " + lastName);
+		System.out.println("Age: " + age);
+
 
 	}
 
