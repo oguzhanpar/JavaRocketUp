87ecbda1c322e65a8f7c1b1f4f459ced77697d95
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-28 20:59:58 +0300

    --DoWhileLoop--

---
+++ 8-DoWhileLoop/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Do While Loop Starting !");
+
+		int j = 1;
+
+		do {
+
+			System.out.println(j);
+			j++;
+
+		} while (j < 10);
+
+		System.out.println("Do While Loop Completed !");
+
+	}
+
+}
