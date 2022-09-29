f7dd74e17bd082a43dce393f86033450f80df358
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-29 19:23:56 +0300

    --ReCapDemo--

---
+++ 20-ReCapDemo/src/FourOperations.java
@@ -0,0 +1,31 @@
+
+public class FourOperations {
+
+	
+
+	public int sum (int number1, int number2) {
+		
+		return number1 + number2;	
+		
+	}
+	
+	public int multiply(int number1, int number2) {
+		
+		return number1 * number2;	
+		
+	}
+	
+	public int divide(int number1, int number2) {
+		
+		return number1 / number2;
+		
+	}
+	
+public int subtract(int number1, int number2) {
+		
+		return number1 - number2;
+		
+	}
+	
+	
+}
---
+++ 20-ReCapDemo/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		FourOperations fourOperations = new FourOperations();
+
+		int _number1 = 25;
+		int _number2 = 5;
+
+		int _result = fourOperations.divide(_number1, _number2);
+
+		System.out.println("The result is: " + _result);
+
+	}
+
+}
