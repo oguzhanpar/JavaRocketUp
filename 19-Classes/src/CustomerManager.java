00c52cf9e82cf103e77ad50178cd138f37f8c9ed
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-29 19:14:41 +0300

    --Classes--

---
+++ 19-Classes/src/CustomerManager.java
@@ -0,0 +1,28 @@
+
+public class CustomerManager {
+
+
+	
+	public void add() {
+		
+		System.out.println("The customer is added !");
+		
+	}
+	
+	public void delete() {
+		
+		
+		System.out.println("The customer is deleted !");
+		
+	}
+	
+	public void update() {
+		
+		
+		System.out.println("The customer is updated !");
+		
+	}
+		
+	
+
+}
---
+++ 19-Classes/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		CustomerManager customerManager = new CustomerManager();
+		
+		customerManager.add(); // Customer Add 
+		
+		customerManager.delete(); // Customer Delete 
+
+		customerManager.update(); // Customer Delete 
+		
+
+	}
+
+}
