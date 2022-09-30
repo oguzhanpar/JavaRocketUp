1c8162a5060bdbf14ef2e847e39973b750a4f444
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-30 11:07:14 +0300

    --Method Overloading--

---
+++ 24-Method Overloading/src/FourOperations.java
@@ -0,0 +1,17 @@
+
+public class FourOperations {
+
+	public int sum(int number1, int number2) {		
+		
+		return number1 + number2;
+		
+	}
+	
+public int sum(int number1, int number2, int number3) {		
+		
+		return number1 + number2 + number3;
+		
+	}
+	
+
+}
---
+++ 24-Method Overloading/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		FourOperations fourOperations = new FourOperations();
+		int result1 = fourOperations.sum(12, 23);
+		int result2 = fourOperations.sum(2, 1, 19);
+		
+		System.out.println("Result1: " + result1 + " ------ Result2: " + result2);
+		
+
+	}
+
+}
