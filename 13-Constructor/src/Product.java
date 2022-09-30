854387ab3904a64172b0a0c799550a9c9cbc0304
oguzhanpar <oguzhan.parlak@yahoo.com>
2022-09-30 11:01:41 +0300

    --Constructor--

---
+++ 13-Constructor/src/Product.java
@@ -0,0 +1,52 @@
+
+public class Product {
+	
+	
+	public Product() {
+		
+		System.out.println("Constructor works here!");
+		
+	}
+	
+	
+
+		int _id;
+		String _name;
+		String _description;
+		double _price;
+		int _stockAmount;
+		public int get_id() {
+			return _id;
+		}
+		public void set_id(int _id) {
+			this._id = _id;
+		}
+		public String get_name() {
+			return _name;
+		}
+		public void set_name(String _name) {
+			this._name = _name;
+		}
+		public String get_description() {
+			return _description;
+		}
+		public void set_description(String _description) {
+			this._description = _description;
+		}
+		public double get_price() {
+			return _price;
+		}
+		public void set_price(double _price) {
+			this._price = _price;
+		}
+		public int get_stockAmount() {
+			return _stockAmount;
+		}
+		public void set_stockAmount(int _stockAmount) {
+			this._stockAmount = _stockAmount;
+		}
+
+		
+		
+
+}
---
+++ 13-Constructor/src/ProductManager.java
@@ -0,0 +1,10 @@
+
+public class ProductManager {
+
+	public void add(Product product) {
+		
+		System.out.println(product._name + " - Product added !");
+		
+	}
+
+}
